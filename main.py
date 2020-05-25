a68cfc0e489bc9fefc97a1a3313294fd838e71b3
Velismo <juanmanuelcantero@gmail.com>
2020-05-25 14:45:56 +0200

    Create the function respond to answer the question What is your name

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ r = sr.Recognizer()
 
 def record_audio():
     with sr.Microphone() as source:
-        print('Di algo')
         audio = r.listen(source)
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
-            print(voice_data)
         except sr.UnknownValueError:
             print('Lo siento, no te he entendido')
         except sr.RequestError:
             print('Lo siento, el sistema no funciona correctamente')
         return voice_data
+
+print('¿Cómo puedo ayudarte?')
+voice_data = record_audio()
+respond(voice_data)
