886c47ef3737d92a08507b6c0b1a06720cd944a8
Velismo <juanmanuelcantero@gmail.com>
2020-05-25 14:08:56 +0200

    Add some exceptions to the file in case of error

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-    print('Say something')
+    print('Di algo')
     audio = r.listen(source)
-    voice_data = r.recognize_google(audio)
-    print(voice_data)
-    
+    try:
+        voice_data = r.recognize_google(audio)
+        print(voice_data)
+    except sr.UnknownValueError:
+        print('Lo siento, no te he entendido')
