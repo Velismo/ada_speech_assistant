9574534c22213fe8234d15b399df0fe2faa7c7a8
Velismo <juanmanuelcantero@gmail.com>
2020-05-25 14:25:39 +0200

    Create the function record_audio to include the previous code

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print('Di algo')
-    audio = r.listen(source)
-    try:
-        voice_data = r.recognize_google(audio)
-        print(voice_data)
-    except sr.UnknownValueError:
-        print('Lo siento, no te he entendido')
+
+def record_audio():
+    with sr.Microphone() as source:
+        print('Di algo')
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+            print(voice_data)
+        except sr.UnknownValueError:
+            print('Lo siento, no te he entendido')
+        except sr.RequestError:
+            print('Lo siento, el sistema no funciona correctamente')
+        return voice_data
